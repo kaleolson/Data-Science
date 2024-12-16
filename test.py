74f490c25ce85bffe77689c439aa134b334ccc3f
kaleolson <karl1.olsson1@outlook.com>
2024-12-16 14:02:27 +0100

    3

--- test.py
+++ test.py
@@ -1,2 +1,9 @@
-x = 5
+x = 55
 print (x)
+#echo "# Data-Science" >> README.md
+#git init
+#git add README.md
+#git commit -m "first commit"
+#git branch -M main
+#git remote add origin https://github.com/kaleolson/Data-Science.git
+#git push -u origin main//
