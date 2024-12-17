1c639df71d2f4e70521c976cda6beaa724f54a2d
kaleolson <karl1.olsson1@outlook.com>
2024-12-17 15:02:50 +0100

    test

--- test.py
+++ test.py
@@ -1,8 +1,8 @@
-x = 10
+""" x = 10
 y = 20
 z = 10+20
 
-print (z)
+print (z) """
 #echo "# Data-Science" >> README.md
 #git init
 #git add README.md
@@ -10,3 +10,29 @@ print (z)
 #git branch -M main
 #git remote add origin https://github.com/kaleolson/Data-Science.git
 #git push -u origin main//
+#git push --set-upstream origin main kopplar origin till main i git hub
+#shift + alt + A för att kommenter ett helt block
+#shift + alt + N för att köra med Code runner (glöm ej att spara innan)
+""" a = 5+3
+b = 5//2
+c = 5%2
+d = 5**2
+print(a,b,c,d) """
+
+""" text = "Data Science"
+print(text[:1])
+
+text = "Data Science"
+print(text[-1:])
+
+text = "DataScience"
+print(text[:5])
+ """
+""" text = " Python är roligt"
+print(text.strip())
+print(text.replace("roligt","fantastiskt"))
+print(text.upper()) """
+
+age = input("Skriv in din ålder: ")
+namn = input("Skriv in ditt namn: ")
+print(f"Hej {namn}! Du är {age} år gammal..... och jag vet vart du bor")
--- test2.py
+++ test2.py
@@ -16,7 +16,56 @@ sup
 
 #print(type(sträng),type(inta),type(flytning),type(lögn))
 
-x = "123"
-y = 2
-z = int(x)*y
-print(z)
+#x = "123"
+#y = 2
+#z = int(x)*y
+#print(z)
+
+""" a ="Hello, World!"
+b = a.split(",") # returns ['Hello', ' World!']
+print(b)
+
+a = 5+3
+b = 5//2
+c = 5%2
+d = 5**2
+print(a,b,c,d) """
+
+""" x = 10
+y = 3
+
+print(x+y,x-y,x/y,x*y,x%y,x//y,x**y)
+x = 3.14
+print(int(x))
+x = 5
+print(float(x))
+ """
+""" age = 10
+txt = "jag är " + str(age) + " år"
+print(txt)
+txt = f"Jag är {age} år"
+print(txt) """
+
+""" resultat = round(0.1 + 0.2, 1)
+print(resultat)
+
+tal1 = 0.1
+tal2 = 0.2
+tal1 = int(tal1 * 10)
+tal2 = int(tal2 * 10) 
+print(tal1 + tal2)
+korrektSvar = (tal1 + tal2)/10
+print(korrektSvar) """
+
+""" x = input("Skriv ett tal: ")
+x = int(x)
+print (x + 5) """
+
+""" x = input("Skriv första talet: ")
+y = input("Skriv andra talet: ")
+x = float(x)
+y = float(y)
+summa = int(x)+int(y)
+print (f"Summan är : {summa}") """
+
+
