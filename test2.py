7d3b6f07a4df4b6697852dd564078bbe19c3acc9
kaleolson <karl1.olsson1@outlook.com>
2024-12-17 11:08:23 +0100

    alla ska inte med

--- test2.py
+++ test2.py
@@ -3,15 +3,20 @@ sup
 """
 
 
-y = "skola är "
-x = "kul"
-z = "inte kul"
-print (f'''Jag tycker att {y}{x} 
-oftast iallafall''')
+#y = "skola är "
+#x = "kul"
+#z = "inte kul"
+#print (f'''Jag tycker att {y}{x} 
+#oftast iallafall''')
 
-sträng = "Tjo"
-inta = 7
-flytning = 8.0
-lögn = False
+#sträng = "Tjo"
+#inta = 7
+#flytning = 8.0
+#lögn = False
 
-print(type(sträng),type(inta),type(flytning),type(lögn))
+#print(type(sträng),type(inta),type(flytning),type(lögn))
+
+x = "123"
+y = 2
+z = int(x)*y
+print(z)
